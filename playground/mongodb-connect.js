1597886aba23f7448405820e5a0280701458a43b
Wonmi <wonmadesign@gmail.com>
2019-02-26 03:22:22 +0900

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server')
+    }
+    console.log('Connected to MongdoDB Server')
+    
+    const db = client.db('TodoApp')
+    db.collection('Todos').insertOne({
+        text:'Something to do',
+        completed: false
+    }, (err, result) => {
+        if(err) {
+            return console.log('Failed to insert todo', err)
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2))
+        // ops: 모든 document다 갖고있음
+    })
+
+    db.collection('Users').insertOne({
+        name: 'Wonmi',
+        age: 32,
+        location: 'Seoul'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Failed to insert user', err)
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    })
+
+    client.close()
+})
