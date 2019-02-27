67a2d1745208c6151c43a6769ed9a86c90d729c8
Wonmi <wonmadesign@gmail.com>
2019-02-27 23:48:46 +0900

    Added Update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,6 +7,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongdoDB Server')
     
     const db = client.db('TodoApp')
+
     db.collection('Todos').insertOne({
         text:'Something to do',
         completed: false
@@ -18,16 +19,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         // ops: 모든 document다 갖고있음
     })
 
-    db.collection('Users').insertOne({
-        name: 'Wonmi',
-        age: 32,
-        location: 'Seoul'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Failed to insert user', err)
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2))
-    })
+    // db.collection('Users').insertOne({
+    //     name: 'Wonmi',
+    //     age: 32,
+    //     location: 'Seoul'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Failed to insert user', err)
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2))
+    // })
 
     client.close()
 })
---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server')
+    }
+    console.log('Connected to MongdoDB Server')
+
+    //findOneAndUpdate
+    const db = client.db('TodoApp')
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c76970f0f592c2d45c2627f')
+    // }, {
+    //     $set: {completed: true}
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result)
+    // }, (err) => {
+    //     console.log('Failed to fetch docs')
+    // })
+    db.collection('Users').findOneAndUpdate({
+        name: 'Joshua'
+    }, {
+            $set: { name: 'Josh' },
+            $inc: {age: -1}
+        }, {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result)
+        }, (err) => {
+            console.log('Failed to fetch docs')
+        })
+    // client.close()
+})
