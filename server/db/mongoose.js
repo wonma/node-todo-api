66c5fa043478ae483d936638d7f0586bd45bfd82
Wonmi <wonmadesign@gmail.com>
2019-03-01 13:06:49 +0900

    Add GET /todos:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,16 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/TodoApp')
 
-module.exports = {mongoose}
+module.exports.mongoose = {mongoose}
+
+// // 다음 둘의 차이는?
+// module.exports = {
+//     mongoose: mongoose
+// }
+// // 이름이 없는 object로 지정파일명.mongoose로 값에 접근 가능하다.
+
+// module.exports.mongoose = {
+//     mongoose: mongoose
+// }
+// // 이름이 있는 object로 mongoose라는 이름으로 반드시 require해야한다.
+// // 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const { mongoose } = require('./db/mongoose')
+const { ObjectId } = require('mongodb')
+const { mongoose } = require('./db/mongoose') // 이거 왜 써야하지??
+// mongoose.js에서 connection했던 내용들 다 들고와서 오른쪽에서 resolve 됨.
+// 비록 mongoose.~~이런식으로 아래에 쓰이지 않았더라도 
+// '='의 오른쪽에서 필요한 작업을 모두 execution한 것임.
+
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 
@@ -22,12 +27,31 @@ app.post('/todos',(req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => { // todos는 array임
-        res.send({todos})   // 이렇게 { } 한 번 감싸주지 않으면, req.body.todos밖에 접근 못함
-    }, (e) => {             // 미래에 혹시나 다른 prop에 접근 할 수 도 있으므로 오브젝트에 한 번 담아준거임
+        res.send({todos})   
+    }, (e) => {             
         res.status(400).send(e)
     })  
+}) 
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send('id is invalidddddd')
+    } 
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send('hmm 404 error, ID not found')
+        }
+        res.send(todo)
+    }).catch((e) => {
+        res.status(400).send('Bad request! Invalid ID')
+        // possible cause : server disconnection....
+    })
 })
 
+// res.send({todos})이렇게 {}로 담지 않으면, 후에 res.body.todos밖에 접근 못함
+// 미래에 혹시나 다른 prop에 접근 할 수 도 있으므로 오브젝트에 한 번 담아준거임
 
 app.listen(3000, () => {
     console.log('Started Server')
